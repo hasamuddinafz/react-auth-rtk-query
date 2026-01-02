6aa4cd5e562b838e9cbf9d3b97255b70e558f013
Hasamuddin Afzali <hasamuddin.afz@gmail.com>
2026-01-02 15:19:00 +0300

    Initial project setup (frontend + backend structure)

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login/Login";
+function App() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="" element={<Login />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
